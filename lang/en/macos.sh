1562e67ea2aa24455bd7f7aa83990c8859ea9dd7
sayoridev <mliguori798@gmail.com>
2023-12-30 23:27:24 +0100

    finish macos version

---
+++ lang/en/macos.sh
@@ -0,0 +1,7 @@
+chmod +x lang/en/macos.sh
+
+echo "I procede to install brew and spotify"
+sleep 1.5
+wait
+clear
+bash <(curl -sSL https://spotx-official.github.io/run.sh) -Bcd --installmac	
---
+++ lang/it/macos.sh
@@ -0,0 +1,7 @@
+chmod +x lang/it/macos.sh
+
+echo "Procedo a installare brew e Spotify"
+sleep 1.5
+wait
+clear
+bash <(curl -sSL https://spotx-official.github.io/run.sh) -Bcd --installmac	
