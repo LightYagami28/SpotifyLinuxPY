2edeb122356f259df57abb29a85a0c37436bd3ce
sayoridev <mliguori798@gmail.com>
2023-12-30 23:26:43 +0100

    fix run linux and macos

--- SpotifyLinuxPY.py
+++ SpotifyLinuxPY.py
@@ -1,18 +1,31 @@
-#start call import
 import os
-import time
 import subprocess
+import platform
 
-#set path of the script
-enfolder = 'lang/en/linux.py'
-itfolder = 'lang/it/linux.py'
+# Set paths
+base_folder = 'lang'
+en_folder = os.path.join(base_folder, 'en')
+it_folder = os.path.join(base_folder, 'it')
+script_name = 'linux.py'
+script_bash_en = os.path.join(en_folder, 'macos.sh')
+script_bash_it = os.path.join(it_folder, 'macos.sh')
 
-#ask language
-lang=input("Which language do you prefer? \n1. English \n2. Italian \n")
-if lang == "1":
-    subprocess.run(['python', enfolder])
-elif lang == "2":
-    subprocess.run(['python', itfolder])
-else:
-    print("Not a valid choise")
+# Check macOS
+if platform.system() == "Darwin":
+    lang_mac = input("Running on macOS, what language do you want to use?\n1. English\n2. Italian\n")
+    selected_script = script_bash_en if lang_mac == "1" else script_bash_it if lang_mac == "2" else None
+
+    if selected_script:
+        subprocess.run(['bash', selected_script], check=True)
+    else:
+        print("Not a valid choice")
     exit()
+
+# Ask language for non-macOS platforms
+lang = input("Which language do you prefer?\n1. English\n2. Italian\n")
+selected_script = os.path.join(en_folder, script_name) if lang == "1" else os.path.join(it_folder, script_name) if lang == "2" else None
+
+if selected_script:
+    subprocess.run(['python', selected_script])
+else:
+    print("Not a valid choice")
